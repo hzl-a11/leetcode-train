729168066266542e99967386ea7320c36807062a
hzl-a11 <huangzhenglian4@gmail.com>
2026-03-08 11:12:33 +0800

    【中等】1302. 层数最深叶子节点的和

--- Tree/average-of-levels-in-binary-tree.go
+++ Tree/average-of-levels-in-binary-tree.go
@@ -2,8 +2,10 @@
 // 给定一个非空二叉树的根节点 root , 以数组的形式返回每一层节点的平均值。与实际答案相差 10-5 以内的答案可以被接受
 // https://leetcode.cn/problems/average-of-levels-in-binary-tree/description/
 
+package Tree
+
 func averageOfLevels(root *TreeNode) []float64 {
-	levelAvgMap := make(map[int]int) //key:层数 value:该层的节点值列表
+	levelAvgMap := make(map[int]int)   //key:层数 value:该层的节点值列表
 	levelCountMap := make(map[int]int) //key:层数 value:该层的节点数量
 	var dfs func(node *TreeNode, depth int)
 	dfs = func(node *TreeNode, depth int) {
---
+++ Tree/deepest-leaves-sum.go
@@ -0,0 +1,24 @@
+// 1302. 层数最深叶子节点的和
+// 给你一棵二叉树的根节点 root ，请你返回 层数最深的叶子节点的和 。
+// https://leetcode.cn/problems/deepest-leaves-sum/description/
+
+package Tree
+
+func deepestLeavesSum(root *TreeNode) int {
+	maxLevel := 0
+	levelSumMap := make(map[int]int) //key:层数 value:该层的节点值总和
+	var dfs func(node *TreeNode, depth int)
+	dfs = func(node *TreeNode, depth int) {
+		if node == nil {
+			return
+		}
+		if depth > maxLevel {
+			maxLevel = depth
+		}
+		levelSumMap[depth] += node.Val
+		dfs(node.Left, depth+1)
+		dfs(node.Right, depth+1)
+	}
+	dfs(root, 1)
+	return levelSumMap[maxLevel]
+}
