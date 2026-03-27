452f6ceda7fcacbbd17001875fc9fb7d0eda17c9
hzl-a11 <huangzhenglian4@gmail.com>
2026-03-27 10:26:26 +0800

    【中等】39. 组合总和

---
+++ algorithm/back-trace/combination-sum_test.go
@@ -0,0 +1,24 @@
+package backtrace
+
+import "testing"
+
+func Test_combinationSum(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		candidates []int
+		target     int
+		want       [][]int
+	}{
+		{candidates: []int{2, 3, 6, 7}, target: 7},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := combinationSum(tt.candidates, tt.target)
+			// TODO: update the condition below to compare got with tt.want.
+			if true {
+				t.Errorf("combinationSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
