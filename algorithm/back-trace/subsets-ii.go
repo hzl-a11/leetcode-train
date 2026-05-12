e3e2712f1e59cf4cc83f9721808f8c82ac2f8cf6
hzl-a11 <huangzhenglian4@gmail.com>
2026-05-12 12:12:33 +0800

    改了一点

--- algorithm/back-trace/subsets-ii.go
+++ algorithm/back-trace/subsets-ii.go
@@ -7,35 +7,58 @@ package backtrace
 
 import "sort"
 
+// func subsetsWithDup(nums []int) [][]int {
+// 	result := make([][]int, 0)
+// 	trace := make([]int, 0)
+// 	n := len(nums)
+
+// 	//这里的排序很重要，避免顺序不一致导致的问题（即[1,4] [4,1]是一样的）
+// 	// 假设，nums[4,1,4]
+// 	//先确定第一位为4，会加入【4，1】这个结果集
+// 	//然后确定第一位为1，此时仅需考虑idx>=1的位数就可以了，因为idx=0的元素，在前面的选择已经包含到
+// 	//但由于数组支持重复，idx>=1的item的值会和idx=0的值重复，导致出现顺序不一致重复的问题
+// 	//通过排序，可以保证不会出现这种情况，从而保证唯一性
+// 	sort.Ints(nums)
+// 	var backtrace func(idx int)
+// 	backtrace = func(idx int) {
+// 		result = append(result, append([]int(nil), trace...))
+// 		traceMap := make(map[int]struct{}, 0)
+
+// 		for i := idx; i < n; i++ {
+// 			value := nums[i]
+// 			if _, ok := traceMap[value]; ok {
+// 				continue
+// 			}
+// 			traceMap[value] = struct{}{}
+// 			trace = append(trace, value)
+// 			backtrace(i + 1)
+// 			trace = trace[:len(trace)-1]
+
+// 		}
+// 	}
+// 	backtrace(0)
+// 	return result
+// }
+
 func subsetsWithDup(nums []int) [][]int {
+	sort.Ints(nums)
 	result := make([][]int, 0)
 	trace := make([]int, 0)
-	n := len(nums)
-
-	//这里的排序很重要，避免顺序不一致导致的问题（即[1,4] [4,1]是一样的）
-	// 假设，nums[4,1,4]
-	//先确定第一位为4，会加入【4，1】这个结果集
-	//然后确定第一位为1，此时仅需考虑idx>=1的位数就可以了，因为idx=0的元素，在前面的选择已经包含到
-	//但由于数组支持重复，idx>=1的item的值会和idx=0的值重复，导致出现顺序不一致重复的问题
-	//通过排序，可以保证不会出现这种情况，从而保证唯一性
-	sort.Ints(nums)
-	var backtrace func(idx int)
-	backtrace = func(idx int) {
+	var backTrace func(start int)
+	backTrace = func(start int) {
 		result = append(result, append([]int(nil), trace...))
-		traceMap := make(map[int]struct{}, 0)
-
-		for i := idx; i < n; i++ {
-			value := nums[i]
-			if _, ok := traceMap[value]; ok {
+		for i := start; i < len(nums); i++ {
+			// 找第一位不重复的元素去做选择
+			if i > start && nums[i] == nums[i-1] {
 				continue
 			}
-			traceMap[value] = struct{}{}
-			trace = append(trace, value)
-			backtrace(i + 1)
+			trace = append(trace, nums[i])
+			backTrace(i + 1)
 			trace = trace[:len(trace)-1]
-
 		}
+
 	}
-	backtrace(0)
+	backTrace(0)
 	return result
+
 }
--- algorithm/back-trace/subsets.go
+++ algorithm/back-trace/subsets.go
@@ -7,21 +7,17 @@ package backtrace
 
 // 思路：先找包含num[0]的所有子集=num[0]+剩余的item子集合。包含num[0]的所有子集+剩余的item子集合=结果
 func subsets(nums []int) [][]int {
-	var backtrace func(idx int)
-	n := len(nums)
-	r := make([][]int, 0)
-	backtrace = func(idx int) {
-		if idx == n-1 {
-			r = [][]int{{nums[idx]}, nil}
-			return
-		}
-		// 包含num[0]的所有子集=num[0]+剩余的item子集合
-		backtrace(idx + 1)
-		for i := range r {
-			newItem := append(r[i], nums[idx])
-			r = append(r, newItem)
+	res := make([][]int, 0)
+	trace := make([]int, 0)
+	var backtrace func(start int)
+	backtrace = func(start int) {
+		res = append(res, append([]int{}, trace...))
+		for i := start; i < len(nums); i++ {
+			trace = append(trace, nums[i])
+			backtrace(i + 1)
+			trace = trace[:len(trace)-1]
 		}
 	}
 	backtrace(0)
-	return r
+	return res
 }
